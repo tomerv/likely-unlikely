f0f14ca10700b42da8f9c064718d5b4812a146cd
Tomer Vromen <tomerv@gmail.com>
2022-08-18 14:55:00 +0000

    Use Intel syntax for assembly output

--- snippets/process.sh
+++ snippets/process.sh
@@ -13,7 +13,7 @@ for d in *; do
         mkdir -p out/$dd/
 	origdir=$(realpath $dd)
 	pushd out/$dd > /dev/null
-        g++ -c $origdir/snippet.cpp -O3 -S -fdump-tree-all-graph
+        g++ -c $origdir/snippet.cpp -O3 -S -fdump-tree-all-graph -masm=intel
         $scriptdir/filter_dot.sh <snippet.cpp.048t.profile_estimate.dot > snippet.cpp.048t.profile_estimate.filtered.dot
 	dot ./snippet.cpp.048t.profile_estimate.filtered.dot -Tpng > estimate.png
         $scriptdir/filter_dot.sh <snippet.cpp.244t.optimized.dot > snippet.cpp.244t.optimized.filtered.dot
