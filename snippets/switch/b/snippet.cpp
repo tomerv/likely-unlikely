cce41b128a596b6d6d0c62274bc7d6dd5a935334
Tomer Vromen <tomerv@gmail.com>
2022-08-18 14:55:36 +0000

    Add another snippet to switch example

---
+++ snippets/switch/b/snippet.cpp
@@ -0,0 +1,42 @@
+int foo0();
+int foo1();
+int foo2();
+int foo3();
+int foo4();
+int foo5();
+int foo6();
+int foo7();
+int foo8();
+int foo9();
+int bar(int);
+
+int foo(int x)
+{
+    switch (x)
+    {
+        case 0:
+            return foo0();
+        case 1:
+            return foo1();
+        case 2:
+            return foo2();
+        case 3:
+            return foo3();
+        case 4:
+            return foo4();
+        [[unlikely]] case 5:
+            return foo5();
+        case 6:
+            return foo6();
+        case 7:
+            return foo7();
+        case 8:
+            return foo8();
+        case 9:
+            return foo9();
+        [[likely]] default:
+            return bar(x);
+
+    }
+}
+
